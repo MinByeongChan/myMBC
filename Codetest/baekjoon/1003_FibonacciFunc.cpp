7be3cd8214bee50003061e7ae7f721e8f53d669a
MinByeongChan <mbc0481@naver.com>
2019-11-19 22:05:41 +0900

    add

--- Codetest/baekjoon/1003_FibonacciFunc.cpp
+++ Codetest/baekjoon/1003_FibonacciFunc.cpp
@@ -1,41 +1,44 @@
 #include <iostream>
-
 using namespace std;
 
-int n, T, cnt_0, cnt_1;
-int dp[40];
+int dp1[41];
+int dp2[41];
 
-int fibonacci(int n) {
-    if (n == 0) {
-        cnt_0++;
-        return 0;
-    } else if (n == 1) {
-        cnt_1++;
-        return 1;
-    }
-	if(dp[n] != 0 )
-		return dp[n];
-	else {
-        return dp[n] = fibonacci(n-1) + fibonacci(n-2);
-    }
+int fibo_0(int x) {
+	if (x == 0) return 0;
+	if (x == 1) return 1;
+	if (x == 2) return 1;
+	if (dp1[x] != 0) return dp1[x];
+
+	return dp1[x] = fibo_0(x - 1) + fibo_0(x - 2);
 }
 
-int main(int argc, char** argv) {	
-	cin >> T;
-	
-	for(int t=0; t<T; t++) {
-		cin >> n;
-		cnt_1 = 0;
-		cnt_0 = 0;
-		fill(dp, dp+40, 0);
-		
-		cout <<fibonacci(n) << "\n";
-		
-		cout << cnt_0 << " " << cnt_1 << "\n";
-	}
-	
+int fibo_1(int y) {
+	if (y == 0) return 1;
+	if (y == 1) return 0;
+	if (y == 2) return 1;
+	if (y == 3) return 1;
+	if (dp2[y] != 0) return dp2[y];
+	return dp2[y] = fibo_1(y - 1) + fibo_1(y - 2);
+}
 
-	cin >> T;
+int main() {
+	int t;
+	cin >> t;
+
+	fill(dp1, dp1 + 41, 0);
+	fill(dp2, dp2 + 41, 0);
+
+
+	for (int i = 0; i < t; i++) {
+		int a;
+		cin >> a;
+		cout << fibo_1(a) << " " << fibo_0(a) << "\n";
+	}
+
+
+	int n;
+	cin >> n;
 
 	return 0;
 }
