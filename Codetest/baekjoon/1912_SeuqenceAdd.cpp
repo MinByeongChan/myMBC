00a834b794d7129fb6988f7da23d5ed023b1fac9
MBC <mbc0481@naver.com>
2019-12-10 13:35:56 +0900

    Update 1912_SeuqenceAdd.cpp

--- Codetest/baekjoon/1912_SeuqenceAdd.cpp
+++ Codetest/baekjoon/1912_SeuqenceAdd.cpp
@@ -1,58 +1,73 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-//각 케이스마다으 합에 대한 최대값 반환 함수
-int MAX(int t[]){
-	int max = 0;
-	for(int i=0; i<10001; i++){
-		if(t[i] != 0){
-			if(max <= t[i]) max = t[i];
-		}
-	}
-	return max;
+// variable init
+vector<long long int> dp;
+vector<long long int> v;
+int N;
+long long int mx = -1000;
+
+/*
+스택오버플로우로 인한 실패
+N이 5000이상이면 스택이 쌓여 도중에 중단됨.
+*/
+void BinarySearch1(int i, long long int sum) {
+
+	long long int sum_temp = sum + v[i];
+
+	if (sum_temp < dp[i])
+		return;
+	
+	// Update dp[i](max value)
+	if (sum_temp > dp[i]) 
+		dp[i] = sum_temp;
+
+	// Searching for max value
+	if (dp[i] > mx)
+		mx = dp[i];
+	
+	if (i > N - 2)
+		return;
+
+	BinarySearch1(i + 1, dp[i]);
+
+	if (v[i + 1] < 0 && i < N-3)
+		BinarySearch1(i + 2, 0);	
 }
 
-int main(int argc, char** argv) {
-	int n; //n개의 정수로 이루어진 수열
-	scanf("%d", &n);
+int main() {
+	cin >> N;
+	v.resize(N);
 
-	int *a = new int[n]; // 동적으로 배열 생성
-	int temp[10001] = {0,}; // 각 항들을 초기화
-	int result = 0;
-	int x;
+	for (int i = 0; i < N; i++)
+		cin >> v[i];
 	
-	//항이 n개인 수열 생성	
-	for(int i=0; i<n; i++){
-		a[i] = 0;
-		scanf("%d", &a[i]);
-	}
+
+	// duplicate Input vector
+	dp = v;
+
+
+	//Binary search
+	//BinarySearch(0, 0);
 	
-	for(int j=1; j<n-1; j++){
-		int k=0;
-		int p=j;
-		for(int i=0; i<n-1; i++){
-			if(i >= n-j) break; // i값을 제한하기 위함 
-			for(int k=i; k<=p; k++){
-				temp[x] += a[k];
-			}
-			p++;
-			x++;
+	if (dp[0] > mx)
+		mx = dp[0];
+
+	for (int i = 1; i < N; i++) {
+
+		if (dp[i - 1] > 0 && dp[i] + dp[i - 1] > 0) {
+			dp[i] += dp[i - 1];
 		}
+		//cout << "dp[" << i << "] : " << dp[i] << endl;
+
+		if (mx < dp[i])
+			mx = dp[i];
 	}
-	
-	printf("%d\n", MAX(temp));
-	
-	x=0;
-	
-	//각 케이스마다 저장되어있는 값들을 출력
-	for(int i=0; i<10001; i++){
-		if(temp[i] != 0) printf("%d : %d\n", i+1, temp[i]);
-	}
-	
-	scanf("%d", &n);
-	
-	delete[] a;
-	
+
+	cout << mx;
+
+	cin >> N;
 	return 0;
 }
+
