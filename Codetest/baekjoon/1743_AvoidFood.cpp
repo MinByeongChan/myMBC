4ca1031a26b32d63f39eb9b828cb8cd600aec3ca
MBC <mbc0481@naver.com>
2019-12-30 15:16:28 +0900

    Update 1743_AvoidFood

--- Codetest/baekjoon/1743_AvoidFood.cpp
+++ Codetest/baekjoon/1743_AvoidFood.cpp
@@ -1,57 +1,87 @@
 #include <iostream>
-#include <cstring>
-
+#include <vector>
 using namespace std;
 
-int N, M; // N: 세로, M: 가로 
-int K; //음식물 개수 
-int dx[] = {1, 0, -1, 0};
-int dy[] = {0, 1, 0, -1};
-int arr[101][101] = {0, };
+class Gragh {
+public:
+	int n, m;
+	vector<vector<int> > arr; //이중벡터로 선언
 
-//DFS
-int dfs(int y, int x){
-	//쓰레기 위치(1)만 DFS탐색 시작
-	if(arr[y][x] != 1) return 0;
-	int max = 0; // DFS크기위 최대값
-	arr[y][x] = 2; // 방문 표시
-	printf("(%d, %d)\n", y, x); // 출력 
-	for(int i=0; i<4; i++){
-		int nextX = x+dx[i];
-		int nextY = y+dy[i];
-		dfs(nextY, nextX);
-		max++;
-	}
-	return max;
-}
+	// 보조생성자
+	Gragh(int m1, int n1): m(m1), n(n1){
+		arr.resize(m+1);
+		for (int i = 0; i < m+1; i++) {
+			arr[i].resize(n+1);
+		}
+	};
 
-int main(int argc, char** argv) {
-	int r=0, c=0; //r: 세로, c: 가로 
-	int result = 0;
-	int temp=0;
-	cin >> N >> M >> K; //입력
-	//범위 초과
-	if(K < 1 || K>10000 || N < 1 || M < 1) return 0;
-	
-	//음식물쓰레기 위치 설정(좌표)
-	for(int i=0; i<K; i++){
-		cin >> r >> c;
-		arr[r][c] = 1;
+	int dfs(int x, int y) {
+		int direct[4][2] = { {1,0}, {0,1}, {-1,0}, {0,-1} };
+		int size = 1; // 사이즈 증감자
+
+		arr[x][y] = 0; // 노드를 방문했다고 표시
+
+		for (int i = 0; i < 4; i++) {
+			int nextX = x + direct[i][0];
+			int nextY = y + direct[i][1];
+
+			if (nextX < 1 || nextX > m || nextY < 1 || nextY > n)
+				continue;
+
+			//영역을 탐색하면서 1인 경우는 size를 1씩 증가시킴
+			if (arr[nextX][nextY] == 1) {
+				size += dfs(nextX, nextY);
+			}
+				
+			
+		}
+
+		//영역의 size 반환
+		return size;
 	}
-	
-	//DFS실행
-	for(int i=1; i<N; i++){
-		for(int j=1; j<M; j++){
-			if(arr[i][j] == 1){
-				cout << "--- DFS ---" << endl;
-				temp = dfs(i, j);
-				if(result <= temp)
-					result = temp;
+
+	int maxArea() {
+		int mx = 0; // 최대값
+
+		for (int i = 1; i < n+1; i++) {
+			for (int j = 1; j < m+1; j++) {
+				//arr이 1일때만 동작
+				if (arr[j][i] == 1) {
+					//dfs동작
+					int tmpArea = dfs(j, i);
+
+					//최대값 갱신
+					if ( tmpArea > mx ) {
+						mx = tmpArea;
+					}
+				}
 			}
 		}
+
+		return mx;
 	}
-	//DFS크기의 최대값 출력
-	cout << result << endl;
+
+};
+
+
+
+int main(int argc, char** argv) {
+	int N, M, K; // 세로, 가로, 음식물 개수 
+
+	//INPUT
+	cin >> N >> M >> K; 
+	if(K < 1 || K>10000 || N < 1 || M < 1) return 0;
 	
+	Gragh G(M, N);
+
+	for(int i=0; i<K; i++){
+		int r, c;
+		cin >> r >> c;
+		G.arr[c][r] = 1;
+	}
+	
+	//OUTPUT
+	cout << G.maxArea() << "\n";
+
 	return 0;
 }
