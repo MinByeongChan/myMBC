882e310922761803c937774f775029504e2c93e0
MBC <mbc0481@naver.com>
2020-06-26 23:43:24 +0900

    Update React

--- ReactRepo/src/App.js
+++ ReactRepo/src/App.js
@@ -1,35 +1,30 @@
-import React from 'react';
+import React from "react";
 
 //import "./App.css";
-import TodaysPlan from './03/TodaysPlan';
-import MyComponent from './03/MyComponent';
-import PropComponenet from './03/PropsComponenet';
-import ChildComponent from './03/ChildComponent';
-import BooleanComponent from './03/BooleanComponent';
-import ChildComponent2 from './03/ChildComponent2';
-import DefaultPropsComponent from './03/DefaultPropsComponent';
-import ChildProperty from './03/ChildProperty';
-import StateExample from './03/StateExample';
-import ForceUpdateExample from './03/ForceUpdateExample';
-import Counter from './03/Counter';
-import NewCounter from './03/NewCounter';
+import TodaysPlan from "./03/TodaysPlan";
+import MyComponent from "./03/MyComponent";
+import PropComponenet from "./03/PropsComponenet";
+import ChildComponent from "./03/ChildComponent";
+import BooleanComponent from "./03/BooleanComponent";
+import ChildComponent2 from "./03/ChildComponent2";
+import DefaultPropsComponent from "./03/DefaultPropsComponent";
+import ChildProperty from "./03/ChildProperty";
+import StateExample from "./03/StateExample";
+import ForceUpdateExample from "./03/ForceUpdateExample";
+import Counter from "./03/Counter";
+import NewCounter from "./03/NewCounter";
+import ListExample from "./03/ListExample";
 
-function App() extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {count: 10};
-    this.resetCount = this.resetCount.bind(this);
+class App extends React.Component {
+  render() {
+    return (
+      <div className="body">
+        <div>
+          <ListExample />
+        </div>
+      </div>
+    );
   }
-
-  resetCount(){
-    this.setState(({count}) => ({count: count+10}));
-  }
-  return (
-    <div className="body">
-      <div><Counter count={this.state.count}/></div>
-
-    </div>
-  );
 }
 
 export default App;
