0b295cd0921c504cb8b16a77077bcfad9bfd3df5
ILikeTteokBoKki <38879025+MinByeongChan@users.noreply.github.com>
2021-03-11 17:33:47 +0900

    동적 라우팅 적용

--- Frontend/Nextjs/my-nextjs-app/pages/search.js
+++ Frontend/Nextjs/my-nextjs-app/pages/search.js
@@ -1,8 +1,8 @@
-import React from "react";
-import Layout from "../comps/Layout";
+// import React from "react";
+// import Layout from "../comps/Layout";
 
-const Search = ({ url }) => {
-  return <div>당신이 검색한 키워드는 "{url.query.keyword}" 입니다.</div>;
-};
+// const Search = ({ url }) => {
+//   return <div>당신이 검색한 키워드는 "{url.query.keyword}" 입니다.</div>;
+// };
 
-export default Search;
+// export default Search;
