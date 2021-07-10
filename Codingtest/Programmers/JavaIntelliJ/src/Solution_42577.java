ddb7ecbb1caf8da63f500ab57b03705e7d497703
MBC <mbc0481@naver.com>
2021-07-10 20:47:15 +0900

    Programmers 42577 Code Update

--- Codingtest/Programmers/JavaIntelliJ/src/Solution_42577.java
+++ Codingtest/Programmers/JavaIntelliJ/src/Solution_42577.java
@@ -10,52 +10,29 @@
     전화번호부에 적힌 전화번호를 담은 배열 phone_book 이 solution 함수의 매개변수로 주어질 때, 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요.
 * */
 
+import java.util.*;
+
 public class Solution_42577 {
-    static String[] arr = {};
     static int LEN = 0;
 
-    public static boolean SearchingForText(int start) {
-        int startLen = arr[start].length();
-        String startStr = arr[start].substring(0, startLen);
-
-
-
-        for(int i=0; i<LEN; i++) {
-            String strTmp = arr[i].substring(0, startLen);
-            if(strTmp.equals(startStr)){
-                return true;
-            }
-        }
-
-        return false;
-    }
     public static boolean solution(String[] phone_book) {
         LEN = phone_book.length;
 
-        arr = phone_book;
+        ArrayList<String> list = new ArrayList<String>();
 
-        for(String str : arr) {
-            System.out.println(str);
+        for(String str : phone_book) {
+            list.add(str);
         }
 
-        for(int i=0; i<LEN; i++) {
-//            if(SearchingForText(i)) {
-//                return true;
-//            }
-            int str1Len = arr[i].length();
-            for(int j=i+1; j<LEN; j++) {
-                if(i == j) continue;
-                int str2Len = arr[j].length();
-                int MINLEN = Math.min(str1Len, str2Len);
+        Collections.sort(list);
 
-                if(arr[i].substring(0, MINLEN).equals(arr[j].substring(0, MINLEN))){
-                    return false;
-                }
-//                String strTmp = arr[i].substring(0, startLen);
-//                if(strTmp.equals(startStr)){
-//                    return true;
-//                }
-            }
+        for(int i=0; i<LEN-1; i++) {
+            int minLen = Math.min(list.get(i).length(), list.get(i+1).length());
+
+            String str1 = list.get(i).substring(0, minLen);
+            String str2 = list.get(i+1).substring(0, minLen);
+
+            if(str1.equals(str2)) return false;
 
         }
 
@@ -65,7 +42,9 @@ public class Solution_42577 {
     public static void main(String[] args) {
 //        String[] phone_book = {"119", "97674223", "1195524421"};
 //        String[] phone_book = {"123", "456", "789"};
-        String[] phone_book = {"12", "123", "1235", "567", "88"};
+//        String[] phone_book = {"202", "12", "123", "1235", "567", "88", "123112321"};
+        String[] phone_book = {"202", "1190", "2020"};
+
         boolean result = solution(phone_book);
         System.out.println("result "+ result);
     }
